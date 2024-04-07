5654e3e160e65230fb1ae7177cf56a2bf4c845e3
Rutvik Manchekar <73109756+rutvik0709@users.noreply.github.com>
2024-04-07 13:18:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// document.getElementById('next').onclick = function(){
+//     let lists = document.querySelectorAll('.item');
+//     document.getElementById('slide').appendChild(lists[0]);
+// }
+// document.getElementById('prev').onclick = function(){
+//     let lists = document.querySelectorAll('.item');
+//     document.getElementById('slide').prepend(lists[lists.length - 1]);
+// }
+
+// $(document).ready(function () {
+//     $('.slide-effect').mouseleave(function () {
+//         $(this).removeClass('clicked');
+//     }).click(function () {
+//         $(this).addClass('clicked').html($(this).html());
+//     });
+// });
